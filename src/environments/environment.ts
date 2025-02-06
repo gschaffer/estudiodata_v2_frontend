021294d5ad77a9af7204b3faafe49c1af85d03a9
schafferg <gustavo.schaffer.91@gmail.com>
2025-02-06 00:25:02 -0300

    se agregan environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+// src/environments/environment.staging.ts
+export const environment = {
+    production: false,
+    apiUrl: 'https://estudiodata-v2-frontend.onrender.com',
+    featureFlag: true
+  };
+  
