c2e2817e9276bf1de9f7406274fab0aee6bedaa7
schafferg <gustavo.schaffer.91@gmail.com>
2025-03-31 00:33:49 -0300

    continuacion desarrollo

--- src/app/sections/principal/principal.component.ts
+++ src/app/sections/principal/principal.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ServiciosComponent } from '../servicios/servicios.component';
+import { InstagramComponent } from '../instagram/instagram.component';
 
 @Component({
   selector: 'app-principal',
-  imports: [ServiciosComponent],
+  imports: [ServiciosComponent, InstagramComponent],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css',
 })
